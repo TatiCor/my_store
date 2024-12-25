5909597ba2f88bc012927d394914d991f10d8390
TatiCor <130614210+TatiCor@users.noreply.github.com>
2024-12-25 17:53:04 -0300

    Nueva migración añadiendo rol al usuario

--- backend/schemas/users.schema.js
+++ backend/schemas/users.schema.js
@@ -3,15 +3,17 @@ const Joi = require('joi');
 const id = Joi.number().integer().positive();
 const email = Joi.string().min(3).required();
 const password = Joi.string().min(8).required();
-/* const role = Joi.string().min(5); para mas adelante usar con permisos */
+const role = Joi.string().min(5).required(); 
 
 const createUserSchema = Joi.object({
     email,
     password,
+    role
 });
 
 const updateUserSchema = Joi.object({
     password,
+    role
 });
 
 const getUserSchema = Joi.object({
